a1fa7bc8d22ec73bd8b02bec15014d9a1e103107
louisgeek <louisgeek@qq.com>
2020-09-02 18:14:03 +0800

    startup

--- library/src/main/java/com/louisgeek/speech/SpeechInitializer.java
+++ library/src/main/java/com/louisgeek/speech/SpeechInitializer.java
@@ -1,6 +1,27 @@
 package com.louisgeek.speech;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.startup.Initializer;
 
-public class SpeechInitializer extends Initializer<> {
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * startup
+ */
+public class SpeechInitializer implements Initializer<_LibraryProvider> {
+    @NonNull
+    @Override
+    public _LibraryProvider create(@NonNull Context context) {
+        _LibraryProvider.initAppContext(context);
+        return _LibraryProvider.getInstance();
+    }
+
+    @NonNull
+    @Override
+    public List<Class<? extends Initializer<?>>> dependencies() {
+        return Collections.emptyList();
+    }
 }
--- library/src/main/java/com/louisgeek/speech/_LibraryProvider.java
+++ library/src/main/java/com/louisgeek/speech/_LibraryProvider.java
@@ -5,26 +5,36 @@ import android.content.Context;
 /**
  * Created by louisgeek on 2018/9/20.
  */
- public class _LibraryProvider {
+public class _LibraryProvider {
+
     private static Context mAppContext;
-    private static boolean mDebug;
 
     /**
-     * LibInitSpeechContentProvider 调用
      * @param context
      */
     public static void initAppContext(Context context) {
         mAppContext = context.getApplicationContext();
     }
-    public static void initDebug(boolean debug) {
-        mDebug = debug;
-    }
+
     public static Context provideAppContext() {
         return mAppContext;
     }
 
-    public static boolean provideDebug() {
-        return mDebug;
+    //======================================================
+    private static volatile _LibraryProvider sInstance;
+
+    private _LibraryProvider() {
     }
 
+    public static _LibraryProvider getInstance() {
+        if (sInstance == null) {
+            synchronized (_LibraryProvider.class) {
+                if (sInstance == null) {
+                    sInstance = new _LibraryProvider();
+                }
+            }
+        }
+        return sInstance;
+    }
+    //======================================================
 }
